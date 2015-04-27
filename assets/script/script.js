d6fb1ab43e5d9c4a9dcf722c6706185442985968
Tiffany Kim <tiffanyrkim@berkeley.edu>
2015-04-27 15:53:48 -0700

    done...

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+
+    $( window ).load(function(){
+    $(".Kind").delay(200).fadeIn(900);
+    });
+
+    $( window ).load(function(){
+    $(".Choice").delay(500).fadeIn(900);
+    $(".Choice").delay(500).fadeIn(900);
+    });
+
+    $( window ).load(function(){
+    $("#pictures").delay(200).fadeIn(900);
+    });
+
+    $( window ).load(function(){
+    $(".doodle").delay(1000).fadeIn(900);
+    $(".doodle").addClass("rotated");
+
+    });
+
+
 	$(".names").hover(function() {
 		$(".names").css("opacity", "0.3");
 		$(this).css("opacity", "1");
@@ -8,9 +29,24 @@ $(document).ready(function() {
 	}
 	);
 
-	$(".doodle").click(function() {
-		var theta = $(window).scrollTop() % Math.PI;
-		$(".doodle").css({ transform: 'rotate(' + theta + 'rad)' });
+	$(".item").hover(function() {
+		$(this).css("opacity", "1");
+	}, function() {
+		$(this).css("opacity", "0.7");
 	});
+
+    $("#topphoto").hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
+
+    $("#bottomphoto").hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
 
 });
